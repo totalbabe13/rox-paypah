ebf5c0b9423be94aa924bc65ea36e254daecb313
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-08-30 20:59:09 -0500

    script randomly returns rocks, paper, or scissors

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-alert('Yolo Binches!')
+//alert('Yolo Binches!')
+
+
+function computerPlay() {
+  let x = Math.floor(Math.random() * (3) + 1);
+  switch (x) {
+    case 1:
+      return 'Rocks';
+      break;
+
+    case 2:
+      return 'Paper';
+      break;
+
+    case 3:
+      return 'Scissors';
+      break;     
+  }
+};
