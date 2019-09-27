384bb7cdbb5852fad13d8de101050af566faf4fc
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-09-26 22:45:54 -0500

    updated js script now updates html with score count and outcome info, and has ver rough css grid

--- scripts.js
+++ scripts.js
@@ -15,33 +15,50 @@ const scriptNode = document.querySelector("script");
 document.body.insertBefore(scoreDiv,scriptNode);
 document.body.insertBefore(buttonDiv,scoreDiv);
 buttonDiv.style.cssText = 'border: 1px solid red; height: 40vh; display: grid; grid-template-columns: 1fr 1fr 1fr;grid-column-gap: 1vh;';
-scoreDiv.style.cssText  = "border: 1px solid blue; height: 40vh; display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 1fr 1fr;"
+scoreDiv.style.cssText  = "border: 1px solid blue; height: 40vh; display: grid; grid-template-columns: 1fr 1fr; grid-template-rows: 1fr 1fr 1fr;"
 
 let rockBtn = document.createElement("button");
 rockBtn.setAttribute("id","rock");
 rockBtn.textContent= "Rock";
-rockBtn.style.cssText = "border-radius: 50%; margin-top: 6vh; margin-bottom: 6vh;"
+rockBtn.style.cssText = "border-radius: 50%; margin-top: 6vh; margin-bottom: 6vh;";
 
 let paperBtn   = document.createElement("button");
 paperBtn.setAttribute("id","paper")
 paperBtn.textContent = "Paper"
-paperBtn.style.cssText = "border-radius: 50%; margin-top: 6vh; margin-bottom: 6vh;"
+paperBtn.style.cssText = "border-radius: 50%; margin-top: 6vh; margin-bottom: 6vh;";
 
 let scissorBtn = document.createElement("button");
 scissorBtn.setAttribute("id","scissor")
 scissorBtn.textContent = "Scissors"
-scissorBtn.style.cssText = "border-radius: 50%; margin-top: 6vh; margin-bottom: 6vh;"
+scissorBtn.style.cssText = "border-radius: 50%; margin-top: 6vh; margin-bottom: 6vh;";
 
 let scoreHeader = document.createElement("h1");
 scoreHeader.setAttribute("id","scoreKeeper");
 scoreHeader.textContent = "S C O R E";
-scoreHeader.style.cssText = "grid-column: 1 / span 2; text-align: center;"
+scoreHeader.style.cssText = "grid-column: 1 / span 2; text-align: center;";
+
+let playerScoreDisplay = document.createElement("span");
+playerScoreDisplay.setAttribute("id","playerScore");
+playerScoreDisplay.textContent = `YOUR SCORE: ${yourScore}`
+playerScoreDisplay.style.cssText = "text-align: center;"
+
+let computerScoreDisplay = document.createElement("span");
+computerScoreDisplay.setAttribute("id","computerScore");
+computerScoreDisplay.textContent = `COMPUTER SCORE: ${computerScore}`
+computerScoreDisplay.style.cssText = "text-align: center;"
+
+let outComes = document.createElement("span");
+outComes.setAttribute("id","outCome");
+outComes.textContent = '';
+outComes.style.cssText = "text-align: center; grid-column: 1 / span 2; text-align: center;"
 
 buttonDiv.appendChild(rockBtn);
 buttonDiv.appendChild(paperBtn);
 buttonDiv.appendChild(scissorBtn);
 scoreDiv.appendChild(scoreHeader);
-
+scoreDiv.appendChild(playerScoreDisplay);
+scoreDiv.appendChild(computerScoreDisplay);
+scoreDiv.appendChild(outComes);
 
 
 
@@ -111,6 +128,9 @@ function game(xChoice) {
   let round = playRound(xChoice, computerPlay());
   console.log(round);
   console.log(` YOUR SCORE IS: --> ${yourScore} | COMPUTER SCORE IS: --> ${computerScore}`);
+  playerScoreDisplay.textContent = `YOUR SCORE: ${yourScore}`
+  computerScoreDisplay.textContent = `COMPUTER SCORE: ${computerScore}`
+  outComes.textContent = round;
 };
 
 rockBtn.onclick = () => game('rocks');
